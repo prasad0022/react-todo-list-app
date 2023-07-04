c5fd073a47b607a14fc1ffb62372a708022506de
Prasad <ppkhose6@gmail.com>
2023-07-04 13:42:24 +0530

    basic format

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = ()=>{
+
+    const element = (
+        <div>
+            <h1>Hello World !</h1>
+            <p>This is React app.</p>
+        </div>
+    )
+
+    return element;
+}
+
+export default App;
